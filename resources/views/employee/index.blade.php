d1eacfc1aefba6fcf89a1931322a80ccce9a2a3f
KristineGraceY <kristineyabut288@gmail.com>
2024-06-07 15:32:34 +0800

    comment

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -37,7 +37,46 @@
                     </div>
                 </div>
 
-
+                <div class="card mt-5 shadow-lg">
+                    <div class="card-header bg-custom bg-gray text-white">
+                        <h4 class="mb-0">Employee List</h4>
+                    </div>
+                    <div class="card-body bg-white">
+                        <table class="table table-bordered table-striped">
+                            <thead class="thead-dark">
+                              <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">DOB</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($employees as $key => $employee)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $employee->first_name }}</td>
+                                    <td>{{ $employee->last_name }}</td>
+                                    <td>{{ $employee->DOB }}</td>
+                                    <td>{{ $employee->phone }}</td>
+                                    <td>
+                                        <a href="{{ route('employee.edit', $employee->id) }}" class="btn btn-warning btn-sm">
+                                            <i class="fa fa-pencil-square-o"></i> Edit
+                                        </a>
+                                        <form action="{{ route('employee.destroy', $employee->id) }}" method="POST" style="display:inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm">
+                                                <i class="fa fa-trash"></i> Delete
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
