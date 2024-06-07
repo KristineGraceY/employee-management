103a638a52f95e074d31e8ac2ebdb1b1d1b0da3f
KristineGraceY <kristineyabut288@gmail.com>
2024-06-07 15:13:48 +0800

    comment

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -6,9 +6,38 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card shadow-lg">
+                    <div class="card-header bg-custom bg-gray text-white">
+                        <h4 class="mb-0">Register Employee</h4>
                     </div>
+                    <div class="card-body bg-white">
+                        <form method="POST" action="{{ route('employee.store') }}">
+                            @csrf
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="first_name">Employee First Name</label>
+                                    <input type="text" class="form-control" id="first_name" name="first_name" required>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="last_name">Employee Last Name</label>
+                                    <input type="text" class="form-control" id="last_name" name="last_name" required>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="DOB">Employee DOB</label>
+                                    <input type="date" class="form-control" id="DOB" name="DOB" required>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="phone">Phone</label>
+                                    <input type="text" class="form-control" id="phone" name="phone" required>
+                                </div>  
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-secondary btn-block">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
-                
+
                     </div>
                 </div>
             </div>
